cf8753e6451d3f3677b8b714116f3eb880bb7dcd
Daniel Morgan <me@danielmorgan.co.uk>
2025-02-28 15:44:21 +0000

    Set up register page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,31 @@
 import { Stack } from "expo-router";
+import { COLORS } from "@/utils/colors";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: COLORS.background,
+        },
+        headerTintColor: "#fff",
+        contentStyle: {
+          backgroundColor: COLORS.background,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="register"
+        options={{
+          title: "Create Account",
+        }}
+      />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { COLORS } from "@/utils/colors";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello world!</Text>
+    <View style={styles.container}>
+      <Link href="/register" asChild>
+        <TouchableOpacity style={styles.outlineButton}>
+          <Text style={styles.outlineButtonText}>Register</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    backgroundColor: COLORS.background,
+  },
+  outlineButton: {
+    marginVertical: 8,
+    padding: 12,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: COLORS.primary,
+    backgroundColor: "transparent",
+    alignItems: "center",
+  },
+  outlineButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
